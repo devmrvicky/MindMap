7ead312d9d78a55b6a5df0551c1af2a2da6df429
Vikash Kumar <devmrvicky.com>
2025-05-08 00:27:10 +0530

    add node evn

--- src/axios/axiosConfig.ts
+++ src/axios/axiosConfig.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    import.meta.env.VITE_NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://mind-map-server.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
